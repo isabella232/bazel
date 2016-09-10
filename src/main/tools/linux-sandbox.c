d4ff150e92bb5c0c0861e2eb357a976462887bc5
Anup Chenthamarakshan <anupc@dropbox.com>
2016-09-10 15:27:26 +0000

    Don't create a new network namespace for each action.

--- src/main/tools/linux-sandbox.c
+++ src/main/tools/linux-sandbox.c
@@ -365,6 +365,10 @@ static void ParseCommandLine(int argc, char *const *argv) {
     }
   }
 
+  // HACK: Creating a new network namespace is super expensive.
+  // Disable creation of a new netns for each action.
+  opt.create_netns = 0;
+
   AddMountSource(NULL);
 
   while (optind < argc && argv[optind][0] == '@') {
