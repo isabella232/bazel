ddc47d59483a12f1e956a70556bcd9e79e305372
Benjamin Peterson <benjamin@dropbox.com>
2017-02-22 18:55:49 +0000

    do not leak scary env vars from the client into actions

--- src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
@@ -75,20 +75,4 @@ public class BazelConfiguration extends Fragment {
     }
     return new PathFragment("/bin/bash");
   }
-
-  @Override
-  public void setupShellEnvironment(ImmutableMap.Builder<String, String> builder) {
-    String path = System.getenv("PATH");
-    builder.put("PATH", path == null ? "/bin:/usr/bin" : path);
-
-    String ldLibraryPath = System.getenv("LD_LIBRARY_PATH");
-    if (ldLibraryPath != null) {
-      builder.put("LD_LIBRARY_PATH", ldLibraryPath);
-    }
-
-    String tmpdir = System.getenv("TMPDIR");
-    if (tmpdir != null) {
-      builder.put("TMPDIR", tmpdir);
-    }
-  }
 }
