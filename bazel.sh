417d0bc603bcdbfd80b2b1a5cabf7955d660ff08
Mike Solomon <msolo@dropbox.com>
2017-03-15 22:40:17 +0000

    Fix default config and prepare for Zulu.
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: herb, naphat
    
    Differential Revision: https://tails.corp.dropbox.com/D275429

--- bazel.sh
+++ bazel.sh
@@ -3,7 +3,7 @@
 # shelby and other shell hosts have java 1.6 as default.
 # Hack around that for now rather than herding the cats for an upgrade.
 # Wonder upon wonders, we now have multiple places to hide the jvm.
-for x in /usr/lib/jvm/java-8-oracle /usr/lib/jvm/jdk-8-oracle-x64;
+for x in /usr/lib/jvm/zulu-8-amd64 /usr/lib/jvm/java-8-oracle /usr/lib/jvm/jdk-8-oracle-x64;
 do
   if [ -d $x ]; then
       export JAVA_HOME=$x
--- mkdeb.sh
+++ mkdeb.sh
@@ -18,18 +18,22 @@ trap cleanup EXIT
 mkdir -p $pkg_dir/usr/bin $pkg_dir/etc/bash_completion.d
 
 # Do some hacking to support multiple java installations.
-cp ./output/bazel $pkg_dir/usr/bin/bazel-bin
+cp ./bazel-bin/src/bazel $pkg_dir/usr/bin/bazel-bin
 cp bazel.sh $pkg_dir/usr/bin/bazel
 chmod +x $pkg_dir/usr/bin/bazel
 
 cp bazel.bazelrc $pkg_dir/usr/bin/bazel-bin.bazel-binrc
 
-./output/bazel --batch build -c opt scripts:bash_completion
+./bazel-bin/src/bazel --batch build -c opt scripts:bash_completion
 cp ./bazel-bin/scripts/bazel-complete.bash $pkg_dir/etc/bash_completion.d/bazel || fail "Can't package bash_completion"
 
 # oracle-java8-jdk
 # oracle-java8-installer
-timestamp=$(python -c "import time; print time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))")
+if [[ "$1" != "" ]]; then
+  timestamp=$1
+else
+  timestamp=$(python -c "import time; print time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))")
+fi
 fpm -d oracle-java8-jdk --verbose --debug --prefix / -C $pkg_dir -s dir -t deb -n "bazel" --provides bazel --conflicts bazel-beta --replaces bazel-beta -v 1.0.$timestamp usr etc
 
 # Beta package
