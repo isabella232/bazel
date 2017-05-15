17d6fb3c85e3a7cc8663d54333ae763e85ec22f4
Alexander Morozov <lk4d4@dropbox.com>
2017-05-15 19:37:28 +0000

    [bazel wrapper] run logpusher as a tool with 'nohup'
    Summary: Code is a copy from dropbox logpusher library
    
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Differential Revision: https://phabricator.dropboxer.net/D294885

--- bazel.py
+++ bazel.py
@@ -1,17 +1,66 @@
 #!/usr/drte/v1/python-2.7.7/bin/python2.7 -ESs
 
+import errno
 import getpass
 import json
 import os
+import os.path
 import signal
 import subprocess
 import sys
 import time
+import json
 
-LOG_DIR_BASE='/var/log/dropbox/logpusher'
-LOG_DIR=os.path.join(LOG_DIR_BASE, 'bazel')
 JAVA_HOME_CANDIDATES = ['/usr/lib/jvm/zulu-8-amd64', '/usr/lib/jvm/java-8-oracle', '/usr/lib/jvm/jdk-8-oracle-x64']
 BAZEL_BIN = '/usr/bin/bazel-bin'
+
+LOG_BASE_DIR = '~/.logpusher'
+LOGPUSHER_BIN = '/usr/bin/logpusher'
+
+
+def _spawn_logpusher():
+    with open(os.devnull, 'w') as FNULL:
+        subprocess.Popen(['nohup', LOGPUSHER_BIN],
+                         stdin=None, stdout=FNULL, stderr=FNULL, close_fds=True)
+
+
+def _mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def write_metrics(name, data):
+    """
+    This func writes log file on disk in
+    ~/.logpusher/<name>/<timestamp>-<name>.log.
+    Then it spawns logpusher to collect logs in background.
+    If there is no logpusher in $PATH, this function is no-op.
+    There must be key in data which is equal to name - it's application data. If there is no such
+    key KeyError will be raised.
+    """
+    if not os.path.isfile(LOGPUSHER_BIN):
+        return
+    # check that name field (application data) is exists in data dictionary
+    data[name]
+    log_dir = os.path.expanduser(os.path.join(LOG_BASE_DIR, name))
+    try:
+        _mkdir_p(log_dir)
+    except Exception as e:
+        print >>sys.stderr, 'WARNING: Unable to create path for logpusher: %s' % (e)
+        print >>sys.stderr
+        # no point to do anything if dir is not created
+        return
+    write_time = time.time()
+    log_filename = '%s-%.6f.log' % (name, write_time)
+    data['posted_timestamp'] = int(write_time)
+    write_metrics_log(log_dir, log_filename, data)
+    # start logpusher to collect, it will just exit if one already running
+    _spawn_logpusher()
 
 
 def get_bazel_bin():
@@ -59,21 +108,6 @@ def detect_parent_process():
 
   return parent_proc
 
-def setup_log_dir():
-  if not os.path.isdir(LOG_DIR_BASE):
-    return None
-
-  try:
-    if not os.path.isdir(LOG_DIR):
-      os.mkdir(LOG_DIR)
-    return LOG_DIR
-  except Exception, e:
-    # In case we're unable to test for presence of log dir or create it, silently ignore the failure
-    print >>sys.stderr, 'WARNING: Exception while listing/creating %s: %s' % (LOG_DIR, e)
-    print >>sys.stderr
-    return None
-
-
 def set_java_home():
   for jh in JAVA_HOME_CANDIDATES:
     if os.path.isdir(jh):
@@ -106,12 +140,9 @@ def main():
     'bazel': {
         'cmd': sys.argv,
     },
-    'posted_timestamp': int(start),
   }
 
   set_java_home()
-  log_dir = setup_log_dir()
-  log_filename = 'bazel-%.6f.log' % (start)
 
   metrics['debs'] = detect_versions()
 
@@ -131,7 +162,7 @@ def main():
   metrics['bazel']['duration_ms'] = int((end-start)*1000)
   metrics['bazel']['exit_code'] = returncode
   metrics['bazel']['caller'] = detect_parent_process()
-  write_metrics_log(log_dir, log_filename, metrics)
+  write_metrics('bazel', metrics)
 
   sys.exit(returncode)
 
